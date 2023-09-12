4082983cf349d3025dac6e52f72d2164f638b4f3
Kaushik Kishore <kk12391@gmail.com>
2023-09-12 16:07:40 +0530

    ec2 instance vpc subnets and security groups created

--- first_ec2.tf
+++ first_ec2.tf
@@ -2,7 +2,10 @@
 resource "aws_instance" "myec2" {
   ami           = var.ami
   instance_type = "t2.micro"
-  # subnet_id       = aws_subnet.terraform_subnet.id
+
+  subnet_id              = aws_subnet.terraform-subnet-1.id
+  vpc_security_group_ids = [aws_security_group.terraform_security_group.id]
+
   # security_groups = [aws_security_group.terraform_security_group.name]
   tags = {
     "Name" = "terraform-ec2-instance"
--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "terraformvpc" {
-  cidr_block           = "10.1.0.0/16"
+  cidr_block           = "10.0.0.0/24"
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
@@ -7,12 +7,24 @@ resource "aws_vpc" "terraformvpc" {
   }
 }
 
-# Create multiple subnets within the VPC
-resource "aws_subnet" "terraform_subnet" {
-  vpc_id     = aws_vpc.terraformvpc.id
-  cidr_block = "10.1.0.0/24"
-  tags = {
-    Name = "terraform-subnet"
-  }
+resource "aws_subnet" "terraform-subnet-1" {
+  vpc_id            = aws_vpc.terraformvpc.id
+  cidr_block        = "10.0.0.0/26"
+  availability_zone = "ap-south-1a" # Choose your desired availability zone
+  # Other subnet configuration...
+}
+
+resource "aws_subnet" "terraform-subnet-2" {
+  vpc_id            = aws_vpc.terraformvpc.id
+  cidr_block        = "10.0.0.64/26"
+  availability_zone = "ap-south-1b" # Choose another availability zone
+  # Other subnet configuration...
+}
+
+resource "aws_subnet" "terraform-subnet-3" {
+  vpc_id            = aws_vpc.terraformvpc.id
+  cidr_block        = "10.0.0.128/26"
+  availability_zone = "ap-south-1c" # Choose another availability zone
+  # Other subnet configuration...
 }
 
