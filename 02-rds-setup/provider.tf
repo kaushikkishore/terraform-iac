b33060d7cb1179dd7fabf2181f22ba73cc5a23eb
Kaushik Kishore <kk12391@gmail.com>
2023-09-22 09:36:50 +0530

    moved content to specific folders

---
+++ 02-rds-setup/provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = "1.5.7"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.16.2"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "ap-south-1"
+}
