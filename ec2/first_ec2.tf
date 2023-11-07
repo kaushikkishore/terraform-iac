36fe19501f3e6dfc1f4bb285ca7d5a1d8803bd23
Kaushik Kishore <kk12391@gmail.com>
2023-11-07 15:05:11 +0530

    updated terraform scripts

--- ec2/first_ec2.tf
+++ ec2/first_ec2.tf
@@ -10,33 +10,33 @@ resource "aws_instance" "myec2" {
     "Name" = "terraform-ec2-instance-${local.ec2InstanceName}"
   }
 
-  # SSH connection
-  connection {
-    type        = "ssh"
-    user        = "ec2-user"                         # Replace with the appropriate user for your AMI
-    private_key = file("~/Documents/astuto-mac.pem") # Replace with the path to your private key file
-    host        = aws_instance.myec2.public_ip
-  }
+  # # SSH connection
+  # connection {
+  #   type        = "ssh"
+  #   user        = "ec2-user"                         # Replace with the appropriate user for your AMI
+  #   private_key = file("~/Documents/astuto-mac.pem") # Replace with the path to your private key file
+  #   host        = aws_instance.myec2.public_ip
+  # }
 
-  provisioner "file" {
-    source      = "setup_helm.sh"                # Replace with the path to your local file
-    destination = "/home/ec2-user/setup_helm.sh" # Specify the destination path on the EC2 instance
-  }
-  provisioner "file" {
-    source      = "setup_kubectl.sh"                # Replace with the path to your local file
-    destination = "/home/ec2-user/setup_kubectl.sh" # Specify the destination path on the EC2 instance
-  }
+  # provisioner "file" {
+  #   source      = "setup_helm.sh"                # Replace with the path to your local file
+  #   destination = "/home/ec2-user/setup_helm.sh" # Specify the destination path on the EC2 instance
+  # }
+  # provisioner "file" {
+  #   source      = "setup_kubectl.sh"                # Replace with the path to your local file
+  #   destination = "/home/ec2-user/setup_kubectl.sh" # Specify the destination path on the EC2 instance
+  # }
 
-  # Remote-exec provisioner to run commands on the EC2 instance
-  provisioner "remote-exec" {
-    inline = [
-      "chmod +x /home/ec2-user/setup_helm.sh",
-      "chmod +x /home/ec2-user/setup_kubectl.sh",
-      "sudo /home/ec2-user/setup_kubectl.sh",
-      "sudo /home/ec2-user/setup_helm.sh",
-      "echo 'Configured kubectl and helm'",
-    ]
-  }
+  # # Remote-exec provisioner to run commands on the EC2 instance
+  # provisioner "remote-exec" {
+  #   inline = [
+  #     "chmod +x /home/ec2-user/setup_helm.sh",
+  #     "chmod +x /home/ec2-user/setup_kubectl.sh",
+  #     "sudo /home/ec2-user/setup_kubectl.sh",
+  #     "sudo /home/ec2-user/setup_helm.sh",
+  #     "echo 'Configured kubectl and helm'",
+  #   ]
+  # }
 }
 
 output "publicIP" {
