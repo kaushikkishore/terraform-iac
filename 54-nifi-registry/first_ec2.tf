e2f7ba0db281ca8341160eb2e440938448412d62
Kaushik Kishore <kk12391@gmail.com>
2023-11-08 14:21:03 +0530

    nifi registry not working

--- 54-nifi-registry/first_ec2.tf
+++ 54-nifi-registry/first_ec2.tf
@@ -32,14 +32,26 @@ resource "aws_instance" "nifi-registry" {
       "echo 'export JAVA_HOME=/usr/lib/jvm/java-17-amazon-corretto.x86_64' >> /home/ec2-user/.bashrc",
       "source /home/ec2-user/.bashrc",
       "echo 'Java installed'",
+
       "curl -o nifi-registry-1.23.2-bin.zip https://dlcdn.apache.org/nifi/1.23.2/nifi-registry-1.23.2-bin.zip",
       "unzip nifi-registry-1.23.2-bin.zip",
       "sudo cp -r nifi-registry-1.23.2 /opt/",
       "sudo chown -R ec2-user:ec2-user /opt/nifi-registry-1.23.2",
-      # "/opt/nifi-registry-1.23.2/bin/nifi-registry.sh install",
+
+      "echo -e '\n' >> /opt/nifi-registry-1.23.2/bin/nifi-registry-env.sh",
+      "echo -e '# adding java home' >> /opt/nifi-registry-1.23.2/bin/nifi-registry-env.sh",
+      "echo -e 'export JAVA_HOME=/usr/lib/jvm/java-17-amazon-corretto.x86_64' >> /opt/nifi-registry-1.23.2/bin/nifi-registry-env.sh",
+
+
+      "/opt/nifi-registry-1.23.2/bin/nifi-registry.sh install",
+
+      "echo 'Nifi-registry installed'",
+
       "/opt/nifi-registry-1.23.2/bin/nifi-registry.sh start",
       "sleep 20",
       "/opt/nifi-registry-1.23.2/bin/nifi-registry.sh status",
+      "sleep 10",
+      "/opt/nifi-registry-1.23.2/bin/nifi-registry.sh start",
       "echo 'Finished setting up the apache nifi registry'"
     ]
   }
